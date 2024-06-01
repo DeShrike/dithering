8db8ffa7fcf9c8f4f22a4763b3e2cce6f8b1aa7f
Shrike <de_shrike@hotmail.com>
2024-06-01 11:07:51 +0200

    Implemented ordered dithering

--- dithering.c
+++ dithering.c
@@ -8,6 +8,60 @@
 #include "utils.h"
 #include "image.h"
 
+int dithering2x2[2][2] = {
+   { 0, 2 },
+   { 3, 1 }
+};
+
+int dithering3x3[3][3] = {
+   { 0, 6, 3 },
+   { 7, 5, 1 },
+   { 4, 2, 8 }
+};
+
+int dithering4x4[4][4] = {
+   { 0, 8, 2, 10 },
+   { 12, 4, 14, 6 },
+   { 3, 11, 1, 9 },
+   { 15, 7, 13, 5 }
+};
+
+int dithering8x8[8][8] = {
+   { 0, 32, 8, 40, 2, 34, 10, 42 },
+   { 48, 16, 56, 24, 50, 18, 58, 26 },
+   { 12, 44, 4, 36, 14, 46, 6, 38 },
+   { 60, 28, 52, 20, 62, 30, 54, 22 },
+   { 3, 35, 11, 43, 1, 33, 9, 41 },
+   { 51, 19, 59, 27, 49, 17, 57, 25 },
+   { 15, 47, 7, 39, 13, 45, 5, 37 },
+   { 63, 31, 55, 23, 61, 29, 53, 21 }
+};
+
+Image ordered_dithering(Image input, int matrix_size, int matrix[matrix_size][matrix_size])
+{
+   Image out_image = alloc_image(input.width, input.height);
+
+   int dx = 0;
+   int dy = 0;
+   int r, g, b;
+   for (int x = 0; x < input.width; ++x)
+   {
+      for (int y = 0; y < input.height; ++y)
+      {
+         get_pixel(input, x, y, &r, &g, &b);
+
+         int c = r < 255 / (matrix[dx][dy] + 1) ? 0 : 255;
+
+         set_pixel(out_image, x, y, c, c, c);
+         dx = (dx + 1) % matrix_size;
+      }
+
+      dy = (dy + 1) % matrix_size;
+   }
+
+   return out_image;
+}
+
 Image calc_luminance(Image input)
 {
    Image out_image = alloc_image(input.width, input.height);
@@ -38,6 +92,31 @@ void process(Image input, const char *input_file_path)
    printf("Saving %s\n", output_file_path);
    write_png_file(output_file_path, lum);
 
+   Image d2x2 = ordered_dithering(lum, 2, dithering2x2);
+   Image d3x3 = ordered_dithering(lum, 3, dithering3x3);
+   Image d4x4 = ordered_dithering(lum, 4, dithering4x4);
+   Image d8x8 = ordered_dithering(lum, 8, dithering8x8);
+
+   output_file_path = add_infix(input_file_path, ".2x2");
+   printf("Saving %s\n", output_file_path);
+   write_png_file(output_file_path, d2x2);
+
+   output_file_path = add_infix(input_file_path, ".3x3");
+   printf("Saving %s\n", output_file_path);
+   write_png_file(output_file_path, d3x3);
+
+   output_file_path = add_infix(input_file_path, ".4x4");
+   printf("Saving %s\n", output_file_path);
+   write_png_file(output_file_path, d4x4);
+
+   output_file_path = add_infix(input_file_path, ".8x8");
+   printf("Saving %s\n", output_file_path);
+   write_png_file(output_file_path, d8x8);
+
+   free_image(d8x8);
+   free_image(d4x4);
+   free_image(d3x3);
+   free_image(d2x2);
    free_image(lum);
 }
 
